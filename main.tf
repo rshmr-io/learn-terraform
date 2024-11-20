a106090e4b3bc53e1b6c5bd27506286a723d4ad9
rshmr-io <rishabkumara@gmail.com>
2024-11-20 14:16:26 +0530

    Add module integration

--- main.tf
+++ main.tf
@@ -28,31 +28,16 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_security_group" "blog_sg" {
-  name = "blog_sg"
-  description = "blog security group"
+module "security-group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.2.0"
   vpc_id = data.aws_vpc.default.id
+  name = "blog"
 
-  tags = {
-    Name = "blog"
-  }
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = ["http-80-tcp", "https-443-tcp"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+  
 }
-
-resource "aws_security_group_rule" "http_inbound" {
-  type = "ingress"
-  from_port = 80
-  to_port = 80
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog_sg.id
-}
-
-resource "aws_security_group_rule" "http_outbound" {
-  type = "egress"
-  from_port = 80
-  to_port = 80
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog_sg.id
-}
-
