a7c8014ac112376eb5947bc6ea7fddfc9a947f86
rshmr-io <rishabkumara@gmail.com>
2024-11-20 14:11:15 +0530

    Modify SG name

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_security_group_rule" "http_inbound" {
   to_port = 80
   protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog_sg.id
 }
 
 resource "aws_security_group_rule" "http_outbound" {
@@ -53,6 +53,6 @@ resource "aws_security_group_rule" "http_outbound" {
   to_port = 80
   protocol = "-1"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog_sg.id
 }
 
