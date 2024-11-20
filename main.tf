13ab19cdf308f960c400a055e5adc6907ac5151c
rshmr-io <rishabkumara@gmail.com>
2024-11-20 14:10:20 +0530

    Add Security Groups

--- main.tf
+++ main.tf
@@ -17,8 +17,42 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.blog_sg.id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "blog_sg" {
+  name = "blog_sg"
+  description = "blog security group"
+  vpc_id = data.aws_vpc.default.id
+
+  tags {
+    Name = "blog"
+  }
+}
+
+resource "aws_security_group_rule" "http_inbound" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "http_outbound" {
+  type = "egress"
+  from_port = 80
+  to_port = 80
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
+}
+
