3b1f0296e448eff018dd036a0319c2f29053838e
rshmr-io <rishabkumara@gmail.com>
2024-11-20 15:41:52 +0530

    Create new dev VPC

--- main.tf
+++ main.tf
@@ -19,6 +19,8 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type
   vpc_security_group_ids = [module.security-group.security_group_id]
 
+  subnet_id = module.blog_vpc.public_subnets[0]
+
   tags = {
     Name = "HelloWorld"
   }
@@ -31,7 +33,7 @@ data "aws_vpc" "default" {
 module "security-group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
   name = "blog"
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
@@ -40,4 +42,21 @@ module "security-group" {
   egress_rules = ["http-80-tcp", "https-443-tcp"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 
+}
+
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
